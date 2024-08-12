1e1204591a5380a1d128ac5ac56937005b112a22
soonlee <felix7858@outlook.com>
2024-08-13 00:13:56 +0800

    引擎CSS初步完成，考虑添加插件比如进一步正则处理，添加Xpath解析和支持api的json提取

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -1,89 +1,323 @@
 use getset::Getters;
-use serde_json::Value;
+use reqwest::header::{HeaderMap, HeaderName};
+use reqwest::{Client, RequestBuilder};
+use scraper::{ElementRef, Html, Selector};
+use serde_json::{Map, Value};
+use std::collections::HashMap;
+use std::sync::Arc;
 
-#[derive(Debug)]
-pub(crate) enum SelectorType {
-    CSS,
-    XPath,
-}
-#[derive(Debug)]
-pub(crate) enum SelectorAction {
-    Text,
-    Attribute(String),
-}
 #[derive(Debug, Getters)]
-#[getset(get = "pub")]
-pub(crate) struct Selector {}
-pub(crate) trait Extract {
-    fn output(&self) -> Value;
-}
-#[derive(Debug, Getters)]
-#[getset(get = "pub")]
+#[getset(get = "pub(crate)")]
 pub(crate) struct Mapping {
+    field: String,
     expression: String,
     operation: String,
-    field: String,
+    regexp: Option<String>,
 }
 impl Mapping {
-    pub(crate) fn new(expression: String, operation: String, field: String) -> Self {
+    pub(crate) fn new(
+        field: String,
+        expression: String,
+        operation: String,
+        regexp: Option<String>,
+    ) -> Self {
         Mapping {
+            field,
             expression,
             operation,
-            field,
+            regexp,
         }
     }
+    pub(crate) fn handle_regexp(&self, value: String) {
+        match &self.regexp {
+            Some(regexp) => {
+                let regex = regex::Regex::new(regexp).unwrap();
+                let captures = regex.captures(&value).unwrap();
+                let result = captures.get(1).unwrap().as_str();
+                println!("{}", result);
+            }
+            None => {}
+        }
+    }
+    pub(crate) fn deal_css(&self, element: ElementRef) -> Result<(String, String), String> {
+        match self.operation.as_str() {
+            "text" => {
+                let selector = Selector::parse(&self.expression)
+                    .map_err(|err| format!("解析CSS表达式失败：{}", err))?;
+                let text_ref = element
+                    .select(&selector)
+                    .next()
+                    .ok_or(format!("获取元素失败：{}", self.expression))?;
+                let text = text_ref.text().collect::<String>();
+                Ok((self.field.clone(), text))
+            }
+            _ => {
+                let selector = Selector::parse(&self.expression)
+                    .map_err(|err| format!("解析CSS表达式失败：{}", err))?;
+                let text_ref = element
+                    .select(&selector)
+                    .next()
+                    .ok_or(format!("获取元素失败：{}", self.expression))?;
+                let action = self.operation.replace("@", "");
+                let text = text_ref
+                    .attr(action.as_str())
+                    .ok_or(format!("获取属性失败：{}", action))?;
+                Ok((self.field.clone(), text.to_string()))
+            }
+        }
+    }
+    pub(crate) fn deal_json(&self, value: Value) {}
+    pub(crate) fn deal_xpath(&self) {}
 }
 #[derive(Debug, Getters)]
-#[getset(get = "pub")]
+#[getset(get = "pub(crate)")]
 pub(crate) struct Mapper {
-    expression: String,
-    operation: String,
     field: String,
+    expression: String,
     sequential: bool,
-    children: Vec<Mapper>,
+    mapping: Option<Vec<Mapping>>,
+    children: Option<Vec<Mapper>>,
 }
 impl Mapper {
-    pub(crate) fn new(expression: String, operation: String, field: String, sequential: bool, children: Vec<Mapper>) -> Self {
+    pub(crate) fn new(
+        field: String,
+        expression: String,
+        sequential: bool,
+        mapping: Option<Vec<Mapping>>,
+        children: Option<Vec<Mapper>>,
+    ) -> Self {
         Mapper {
-            expression,
-            operation,
             field,
+            expression,
             sequential,
+            mapping,
             children,
         }
     }
+    pub(crate) fn deal_css(&self, element: ElementRef) -> Result<Value, String> {
+        let selector = Selector::parse(&self.expression)
+            .map_err(|err| format!("解析CSS表达式失败：{}", err))?;
+        let fragment = element
+            .select(&selector)
+            .next()
+            .ok_or(format!("获取元素失败：{}", self.expression))?;
+        match self.sequential {
+            true => {
+                let series = fragment
+                    .child_elements()
+                    .map(|child_element| {
+                        let mut item_fields = Map::<String, Value>::new();
+                        match &self.mapping {
+                            Some(mappings) => {
+                                for mapping in mappings {
+                                    let (field, value) = mapping.deal_css(child_element)?;
+                                    item_fields.insert(field, Value::String(value));
+                                }
+                            }
+                            None => {}
+                        };
+                        match &self.children {
+                            Some(children) => {
+                                for child in children {
+                                    let child_fields = child.deal_css(child_element)?;
+                                    item_fields.insert(child.field.clone(), child_fields);
+                                }
+                            }
+                            None => {}
+                        };
+                        Ok(Value::Object(item_fields))
+                    })
+                    .collect::<Result<Vec<Value>, String>>()?;
+                Ok(Value::Array(series))
+            }
+            false => {
+                let mut fields = Map::<String, Value>::new();
+                match &self.mapping {
+                    Some(mappings) => {
+                        for mapping in mappings {
+                            let (field, value) = mapping.deal_css(fragment)?;
+                            fields.insert(field, Value::String(value));
+                        }
+                    }
+                    None => {}
+                };
+                match &self.children {
+                    Some(children) => {
+                        for child in children {
+                            let child_fields = child.deal_css(fragment)?;
+                            fields.insert(child.field.clone(), child_fields);
+                        }
+                    }
+                    None => {}
+                }
+                Ok(Value::Object(fields))
+            }
+        }
+    }
+    pub(crate) fn deal_json(&self, element: Value) {}
+    pub(crate) fn deal_xpath(&self) {}
 }
 #[derive(Debug, Getters)]
-#[getset(get = "pub")]
+#[getset(get = "pub(crate)")]
 pub(crate) struct Extractor {
-    path: String,
-    parser: String,
-    mapper: Vec<Mapper>,
     field: String,
+    parser: String,
+    mappers: Vec<Mapper>,
 }
 impl Extractor {
-    pub(crate) fn new(path: String, parser: String, mapper: Vec<Mapper>, field: String) -> Self {
+    pub(crate) fn new(field: String, parser: String, mappers: Vec<Mapper>) -> Self {
         Extractor {
-            path,
-            parser,
-            mapper,
             field,
+            parser,
+            mappers,
+        }
+    }
+    pub(crate) fn extract(&self, text: &String) -> Result<Value, String> {
+        match self.parser.as_str() {
+            "CSS" => {
+                let fragment = Html::parse_document(&text);
+                let mut fields = Map::<String, Value>::new();
+                for mapper in &self.mappers {
+                    let value = mapper.deal_css(fragment.root_element())?;
+                    fields.insert(mapper.field.clone(), value);
+                }
+                Ok(Value::Object(fields))
+            }
+            "JSON" => Ok(Value::Null),
+            "XPATH" => Ok(Value::Null),
+            _ => Err("解析器类型错误".to_string()),
         }
     }
 }
 #[derive(Debug, Getters)]
-#[getset(get = "pub")]
+#[getset(get = "pub(crate)")]
+pub(crate) struct Action {
+    method: String,
+    path: String,
+    headers: Option<HashMap<String, String>>,
+    path_params: Option<HashMap<String, String>>,
+    query_params: Option<HashMap<String, String>>,
+    body_params: Option<HashMap<String, String>>,
+    extractor: Arc<Extractor>,
+}
+impl Action {
+    pub(crate) fn new(
+        method: String,
+        path: String,
+        path_params: Option<HashMap<String, String>>,
+        query_params: Option<HashMap<String, String>>,
+        body_params: Option<HashMap<String, String>>,
+        headers: Option<HashMap<String, String>>,
+        extractor: Arc<Extractor>,
+    ) -> Self {
+        Action {
+            method,
+            path,
+            path_params,
+            query_params,
+            body_params,
+            headers,
+            extractor,
+        }
+    }
+    pub(crate) fn deal_headers(&self, request_builder: RequestBuilder) -> RequestBuilder {
+        match &self.headers {
+            Some(headers) => {
+                let mut header_map = HeaderMap::new();
+                headers.iter().for_each(|(key, value)| {
+                    header_map.insert(key.parse::<HeaderName>().unwrap(), value.parse().unwrap());
+                });
+                request_builder.headers(header_map)
+            }
+            None => request_builder,
+        }
+    }
+    pub(crate) fn url(&self) -> String {
+        let mut url = self.path.clone();
+        url = match &self.path_params {
+            Some(path_params) => {
+                for (key, value) in path_params {
+                    url = url.replace(format!("@{}", key).as_str(), value);
+                }
+                url
+            }
+            None => url,
+        };
+        match &self.query_params {
+            Some(query_params) => {
+                let query = query_params
+                    .iter()
+                    .map(|(key, value)| format!("{}={}", key, value))
+                    .collect::<Vec<String>>()
+                    .join("&");
+                format!("{}?{}", url, query)
+            }
+            None => url,
+        }
+    }
+    pub(crate) fn deal_body_params(&self, request_builder: RequestBuilder) -> RequestBuilder {
+        match &self.body_params {
+            Some(body_params) => {
+                let body = body_params
+                    .iter()
+                    .map(|(key, value)| format!("{}={}", key, value))
+                    .collect::<Vec<String>>()
+                    .join("&");
+                request_builder
+                    .body(body)
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+            }
+            None => request_builder,
+        }
+    }
+
+    pub(crate) async fn fetch(&self) -> Result<(), String> {
+        let client = Client::new();
+        let mut request_builder = match self.method.as_str() {
+            "GET" => Ok(client.get(self.url())),
+            "POST" => Ok(client.get(self.url())),
+            _ => Err(format!("不支持的请求方法:{}", &self.method)),
+        }?;
+        request_builder = self.deal_headers(request_builder);
+        request_builder = self.deal_body_params(request_builder);
+        match request_builder.send().await {
+            Ok(response) => {
+                println!("{:?}", response);
+                Ok(())
+            }
+            Err(err) => Err(format!("{:?}", err)),
+        }
+    }
+}
+#[derive(Debug, Getters)]
+#[getset(get = "pub(crate)")]
 pub(crate) struct Spider {
-    origin: String,
-    extractor: Vec<Extractor>,
     field: String,
+    origins: Vec<String>,
+    addresses: Vec<String>,
+    extractors: Vec<Extractor>,
+    actions: Vec<Action>,
 }
 impl Spider {
-    pub(crate) fn new(origin: String, extractor: Vec<Extractor>, field: String) -> Self {
+    pub(crate) fn new(
+        field: String,
+        origins: Vec<String>,
+        addresses: Vec<String>,
+        extractors: Vec<Extractor>,
+        actions: Vec<Action>,
+    ) -> Self {
         Spider {
-            origin,
-            extractor,
             field,
+            origins,
+            addresses,
+            extractors,
+            actions,
         }
+    }
+    pub(crate) fn deal(&self) -> Result<(), String> {
+        for action in &self.actions {
+            println!("{:?}", action);
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,136 @@
-use roxmltree::Document;
-use scraper::{Html, Selector};
-use serde_json::json;
+use crate::core::engine::{Action, Extractor, Mapper, Mapping, Spider};
+use std::sync::Arc;
 
 mod core;
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::new();
-    let action = json!({
-        "url":"https://www.toptoon2.com/page/search/超级"
-    });
-    let url = "https://www.san404.com";
-    let response = client
-        .get(url)
-        .send()
-        .await
-        .unwrap();
-    let text = response.text().await.unwrap();
-    let fragment = Html::parse_document(&text);
-    let comic_selector = Selector::parse("section.container div.content-wrap div.content div.pb div.box").unwrap();
-    let mut comics = vec![comic_selector];
-    let pager_selector = Selector::parse("section.container div.content-wrap div.content div.pagination.pagination-multi ul li:last-child span").unwrap();
-    let pager_text = fragment.select(&pager_selector).next().unwrap().text().collect::<Vec<_>>().join("#");
-    let re = regex::Regex::new(r"(\d+)").unwrap();
-    let pager_num = re.captures(&pager_text).unwrap().get(1).unwrap().as_str().parse::<i32>().unwrap();
-    println!("{}", pager_num);
-    // for i in 2..=pager_num {
-    //     let url = format!("{}/page/{}", &url, i);
-    //     let response = client
-    //         .get(url)
-    //         .send()
-    //         .await
-    //         .unwrap();
-    //     let text = response.text().await.unwrap();
-    //     let fragment = Html::parse_document(&text);
-    //     let comic_selector = Selector::parse("section.container div.content-wrap div.content div.pb div.box").unwrap();
-    //     comics.push(comic_selector);
-    // }
-    // for comic_selector in comics {
-    //     println!("{:?}", &comic_selector);
-    //     for comic in fragment.select(&comic_selector) {
-    //         println!("{:?}", comic.select(&Selector::parse("header p").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("header h2 a").unwrap()).next().unwrap().value().attr("href").unwrap());
-    //         println!("{:?}", comic.select(&Selector::parse("header h2 a").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("header span").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("header small").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("p.text-muted.time").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("p.focus a img").unwrap()).next().unwrap().value().attr("src").unwrap());
-    //         println!("{:?}", comic.select(&Selector::parse("p.note").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //         println!("{:?}", comic.select(&Selector::parse("span.post-tags").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    //     }
-    //     break;
-    // }
-    let num_extractor = core::engine::Extractor::new(action);
-    let comic_mapper = core::engine::Mapper::new("section.container div.content-wrap div.content div.pb div.box".to_string(), "".to_string(), "comics".to_string(), true, vec![
-        core::engine::Mapper::new("header p".to_string(), "text".to_string(), "title".to_string(), false, vec![]),
-        core::engine::Mapper::new("header h2 a".to_string(), "@href".to_string(), "url".to_string(), false, vec![]),
-        core::engine::Mapper::new("header h2 a".to_string(), "text".to_string(), "info1".to_string(), false, vec![]),
-        core::engine::Mapper::new("header span".to_string(), "text".to_string(), "info2".to_string(), false, vec![]),
-        core::engine::Mapper::new("header small".to_string(), "text".to_string(), "info3".to_string(), false, vec![]),
-        core::engine::Mapper::new("p.text-muted.time".to_string(), "text".to_string(), "last_update".to_string(), false, vec![]),
-        core::engine::Mapper::new("p.focus a img".to_string(), "@src".to_string(), "cover".to_string(), false, vec![]),
-        core::engine::Mapper::new("p.note".to_string(), "text".to_string(), "note".to_string(), false, vec![]),
-        core::engine::Mapper::new("span.post-tags".to_string(), "text".to_string(), "tags".to_string(), false, vec![]),
-    ]);
-    let comic_extractor = core::engine::Extractor::new("/".to_string(), "css".to_string());
-    let spider = core::engine::Spider::new("https://www.san404.com".to_string(), vec![], "sansi".to_string());
-    let comic_href = "https://www.san404.com/100895481.html";
-    let response = client
-        .get(comic_href)
-        .send()
-        .await
-        .unwrap();
-    let text = response.text().await.unwrap();
+    let toptoon = "https://www.toptoon2.com/page/search/超级";
+    let spider = Spider::new(
+        "sansiyule".to_string(),
+        vec!["https://www.san404.com".to_string()],
+        vec![],
+        vec![],
+        vec![],
+    );
+    let comics_extractor = Arc::new(Extractor::new(
+        "漫画列表".to_string(),
+        "CSS".to_string(),
+        vec![Mapper::new(
+            "comics".to_string(),
+            "section.container div.content-wrap div.content div.pb".to_string(),
+            true,
+            Some(vec![
+                Mapping::new(
+                    "title".to_string(),
+                    "header p".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "url".to_string(),
+                    "header h2 a".to_string(),
+                    "@href".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "info1".to_string(),
+                    "header h2 a".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "info2".to_string(),
+                    "header span".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "info3".to_string(),
+                    "header small".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "last_update".to_string(),
+                    "p.text-muted.time".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "cover".to_string(),
+                    "p.focus a img".to_string(),
+                    "@src".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "note".to_string(),
+                    "p.note".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+                Mapping::new(
+                    "tags".to_string(),
+                    "span.post-tags".to_string(),
+                    "text".to_string(),
+                    None,
+                ),
+            ]),
+            None,
+        )],
+    ));
+    let pager_extractor = Arc::new(Extractor::new("列表分页".to_string(), "CSS".to_string(), vec![Mapper::new("comics".to_string(), "section.container div.content-wrap div.content div.pagination.pagination-multi ul li:last-child".to_string(), false, Some(vec![
+        Mapping::new("num".to_string(), "span".to_string(), "text".to_string(), Some(r"(\d+)".to_string())),
+    ]), None)]));
+    let mut comics_actions = vec![Action::new(
+        "GET".to_string(),
+        "/".to_string(),
+        None,
+        None,
+        None,
+        None,
+        comics_extractor.clone(),
+    )];
+    for i in 2..=10 {
+        comics_actions.push(Action::new(
+            "GET".to_string(),
+            format!("/page/{}", i),
+            None,
+            None,
+            None,
+            None,
+            comics_extractor.clone(),
+        ));
+    }
+    let mut pager_actions = vec![Action::new(
+        "GET".to_string(),
+        "/".to_string(),
+        None,
+        None,
+        None,
+        None,
+        pager_extractor.clone(),
+    )];
 
-    let fragment = Html::parse_document(&text);
-    let root = fragment.select(&Selector::parse("div.ip").unwrap());
+    let comic_info_extractor = Arc::new(Extractor::new("漫画信息".to_string(), "CSS".to_string(), vec![
+        Mapper::new("comic_info".to_string(), "/".to_string(), true, Some(vec![
+            Mapping::new("cover".to_string(), "section.container div.content-wrap div.content header.article-header div.c-img img".to_string(), "@src".to_string(), None),
+            Mapping::new("title".to_string(), "section.container div.content-wrap div.content header.article-header span.dis".to_string(), "text".to_string(), None),
+            Mapping::new("author".to_string(), "section.container div.content-wrap div.content article.article-content div.article-paging a".to_string(), "text".to_string(), None),
+            Mapping::new("status".to_string(), "section.container div.content-wrap div.content article.article-content p:not(.post-copyright) img".to_string(), "text".to_string(), None),
+        ]), None)]));
+    let comic_info_actions = vec![Action::new(
+        "GET".to_string(),
+        "/100895481.html".to_string(),
+        None,
+        None,
+        None,
+        None,
+        comic_info_extractor.clone(),
+    )];
 
-    // let fragment = Html::parse_document(&text);
-    // println!("{:?}", fragment.select(&Selector::parse("section.container div.content-wrap div.content header.article-header div.c-img img").unwrap()).next().unwrap().value().attr("src").unwrap());
-    // println!("{:?}", fragment.select(&Selector::parse("section.container div.content-wrap div.content header.article-header span.dis").unwrap()).next().unwrap().text().collect::<Vec<_>>().join(" "));
-    // println!("{:?}", fragment.select(&Selector::parse("section.container div.content-wrap div.content article.article-content div.article-paging a").unwrap()).into_iter().map(|x| x.value().attr("href").unwrap().to_string()).collect::<Vec<_>>());
-    // println!("{:?}", fragment.select(&Selector::parse("section.container div.content-wrap div.content article.article-content p:not(.post-copyright) img").unwrap()).into_iter().map(|x| x.value().attr("src").unwrap().to_string()).collect::<Vec<_>>());
-    // let pics = fragment.select(&Selector::parse("section.container div.content-wrap div.content article.article-content p:not(.post-copyright) img").unwrap()).collect::<Vec<_>>();
-    // for (i, pic) in pics.iter().enumerate() {
-    //     println!("{:?}", match pic.value().attr("srcset") {
-    //         Some(x) => x,
-    //         None => pic.value().attr("src").unwrap()
-    //     });
-    // }
-    // let pic_url = "https://imgs.34img.com/2021/004/083552av13280.jpg";
-    // let response = client
-    //     .get(pic_url)
-    //     .header("Referer", "https://www.san404.com/")
-    //     // .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    //     .send()
-    //     .await
-    //     .unwrap();
-    // let bytes = response.bytes().await.unwrap();
-    // std::fs::write("test.jpg", bytes).unwrap();
-    // let pic_url = "https://imgs.34img.com/2021/004/083552av13280-123x300.jpg";
-    // let response = client
-    //     .get(pic_url)
-    //     .header("Referer", "https://www.san404.com/")
-    //     // .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    //     .send()
-    //     .await
-    //     .unwrap();
-    // let bytes = response.bytes().await.unwrap();
-    // std::fs::write("test-123x300.jpg", bytes).unwrap();
+    let html = reqwest::Client::new().get("https://www.san404.com").send().await.unwrap().text().await.unwrap();
+    let pager_extractor = pager_extractor.clone();
+    println!("{:?}", pager_extractor.extract(&html).unwrap());
+    println!("{:?}", comics_extractor.extract(&html).unwrap());
 }
