84ecdef1b654e60f017e247457f8f885b42bd2af
soonlee <soonlee113@163.com>
2024-08-16 17:28:55 +0800

    添加矩型布局组件，并作为页面布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import './App.css';
 import { useTheme } from '@contexts/ThemeProvider';
 import { useLocale } from '@contexts/LocaleProvider';
+import JuLayout from '@containers/JuLayout';
 
 const App = () => {
-const {getTranslation,setLocale,locale} = useLocale();
-const {setTheme,theme} = useTheme();
+  const { getTranslation, setLocale, locale } = useLocale();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <div class="content">
-      <h1>Rsbuild with Solid</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-      <span>{getTranslation("greeting")}</span>
-      <button onClick={()=>setLocale(locale() === "zh-CN" ? "en-US" :"zh-CN")}>{locale() === "zh-CN" ? "en-US" :"zh-CN"}</button>
-      <button onClick={()=>setTheme(theme()==="light" ? "dark":"light")}>{theme()==="light" ? "dark":"light"}</button>
-    </div>
+    <JuLayout
+      header={
+        <div class="content">
+          <button onClick={() => setLocale(locale() === "zh-CN" ? "en-US" : "zh-CN")}>{locale() === "zh-CN" ? "en-US" : "zh-CN"}</button>
+          <button onClick={() => setTheme(theme() === "light" ? "dark" : "light")}>{theme() === "light" ? "dark" : "light"}</button>
+        </div>
+      }
+      main={
+        <div><h1>Rsbuild with Solid</h1>
+          <p>Start building amazing things with Rsbuild.</p>
+          <span>{getTranslation("greeting")}</span></div>
+      } />
   );
 };
 
