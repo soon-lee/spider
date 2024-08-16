710422425cdda199ca1e788d04155893488e456b
soonlee <soonlee113@163.com>
2024-08-16 10:27:12 +0800

    Rsbuild构建Solid+TypeScript项目初始化

---
+++ rsbuild.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginSolid } from '@rsbuild/plugin-solid';
+
+export default defineConfig({
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+    }),
+    pluginSolid(),
+  ],
+  html:{
+    title: '通用爬虫系统',
+    favicon: './src/assets/favicon.ico',
+  }
+});
